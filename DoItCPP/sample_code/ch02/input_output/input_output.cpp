21127cdac6d04d5ffdabdcc83f9c79cba73bc2c6
Groovy52 <kkm5020@gachon.ac.kr>
2026-02-10 15:06:25 +0900

    commit 0210

--- DoItCPP/sample_code/ch02/input_output/input_output.cpp
+++ DoItCPP/sample_code/ch02/input_output/input_output.cpp
@@ -1,7 +1,6 @@
 ﻿// input_output.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-/*
 #include <iostream>
 // 예제: 숫자를 2개 입력받아 합을 출력하기  (41pg)
 int main()
@@ -17,8 +16,8 @@ int main()
 
 	return 0;
 }
-*/
 
+/*
 #include <iostream>
 
 using namespace std; // 네임스페이스 사용 선언 => 하지만 충돌 방지를 위해 std::를 매번 표기하는 코드가 더 좋음.
@@ -41,3 +40,4 @@ int main() // '이 프로그램은 종류 시 정수(int) 하나를 OS(운영체
 	return 0; // 정상적으로 종료됐을 시 OS 에 전달하는 정수값 
 }
 
+*/
